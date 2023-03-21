198b59ed6e903855241c4e4ee9a432794f6b3a8e
devToJu <msg.toju@outlook.de>
2023-03-21 10:40:48 +0100

    js basics: functions and variables

---
+++ jsBasics.js
@@ -0,0 +1,16 @@
+let result;
+
+result = add(10, 5);
+console.log(result);
+
+result = multiply(10, 5);
+console.log(result);
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
